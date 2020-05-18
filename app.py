916e1f0a200b793f5612343588a07b7c52d98da3
fudge88 <f.akhlaq@outlook.com>
2020-05-18 08:34:45 +0000

    secret key and files created

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
 import json
 from flask import Flask, render_template, request, flash
+if os.path.exists('env.py'):
+    import env
+
 
 app = Flask(__name__)
-app.secret_key= 'secretkey'
+app.secret_key = os.environ.get('SECRET_KEY')
 
 
 @app.route('/')
