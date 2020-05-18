06ab4b33254f3c39235c69f8675912c646a91107
fudge88 <f.akhlaq@outlook.com>
2020-05-18 01:48:39 +0000

    contact form flash message set, and message printing complete

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key= 'secretkey'
 
 
 @app.route('/')
@@ -9,9 +11,20 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/recipes')
+def recipes():
+    return render_template('recipes.html', page_title='Recipes')
+
+
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we'll get back to you shortly!".format(request.form["name"]))
+        print(request.form["name"])
+        print(request.form["email"])
+        print(request.form["message"])
+    
+    return render_template('contact.html', page_title='Contact')
 
 
 if __name__ =='__main__':
